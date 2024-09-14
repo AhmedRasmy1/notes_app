dcd21faca80f904cd8cbd6a5d4edd1391bc4ff83
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-14 08:33:19 +0300

    create note model

---
+++ lib/Models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String titel;
+  final String subtitle;
+  final String date;
+  final int color;
+  NoteModel(
+      {required this.titel,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
