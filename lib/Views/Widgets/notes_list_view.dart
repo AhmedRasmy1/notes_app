dd9631b59c169b99be0387094ff8f7777bd74260
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-15 16:37:39 +0300

    The last edit

--- lib/Views/Widgets/notes_list_view.dart
+++ lib/Views/Widgets/notes_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notes_app/Cubits/cubit/notes_cubit.dart';
 import 'package:notes_app/Models/note_model.dart';
 import 'package:notes_app/Views/Widgets/custom_item_note.dart';
@@ -12,17 +13,27 @@ class NotesListView extends StatelessWidget {
     return BlocBuilder<NotesCubit, NotesState>(
       builder: (context, state) {
         List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
-        return ListView.builder(
-            itemCount: notes.length,
-            padding: EdgeInsets.zero,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: NoteItem(
-                  note: notes[index],
-                ),
-              );
-            });
+        return notes.isEmpty
+            ? Center(
+                child: Text(
+                "There are no notes",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: GoogleFonts.acme().fontFamily),
+              ))
+            : ListView.builder(
+                itemCount: notes.length,
+                padding: EdgeInsets.zero,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: NoteItem(
+                      note: notes[index],
+                    ),
+                  );
+                });
       },
     );
   }
